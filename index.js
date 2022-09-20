a33ff693cfe25e4efaae06cbd137d4fbeec4b7c5
Brandon Moreno <brandonm99@me.com>
2022-09-20 10:13:35 -0600

    commit tree f92076f00cef36d1efa7d6f49a1f34945c519dc2

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'it is what is it'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "hi";
+}
